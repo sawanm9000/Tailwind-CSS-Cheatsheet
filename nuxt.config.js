c62a761e08ebffc4c5d15332cf5b547114ac8b98
utopianknight <thesithlord2007+github@gmail.com>
2021-09-15 23:51:07 +0400

    Updated router:base in nuxt-config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: 'Tailwind-CSS-Cheatsheet'
+    base: '/Tailwind-CSS-Cheatsheet/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
