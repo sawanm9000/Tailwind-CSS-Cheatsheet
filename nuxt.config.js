3629cef657608033ad0ec5b5d079c10808b0c7b2
utopianknight <thesithlord2007+github@gmail.com>
2021-09-15 23:42:10 +0400

    Removed router:base from nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: 'Tailwind-CSS-Cheatsheet'
-  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
