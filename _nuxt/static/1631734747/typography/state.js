9dd0c1eb4098ef7aee513a0a7d9b0f7a88584172
utopianknight <utopianknight@users.noreply.github.com>
2021-09-15 19:44:09 +0000

    deploy: 3629cef657608033ad0ec5b5d079c10808b0c7b2

--- _nuxt/static/1631734747/typography/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"Tailwind-CSS-Cheatsheet\u002F_nuxt\u002Fstatic\u002F1631734747",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftypography",config:{_app:{basePath:"Tailwind-CSS-Cheatsheet\u002F",assetsPath:"Tailwind-CSS-Cheatsheet\u002F_nuxt\u002F",cdnURL:a}}}}(null));
