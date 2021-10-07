d817936060d6479f63eedbb047787f06dbd9fdfe
utopianknight <thesithlord2007+github@gmail.com>
2021-10-07 18:44:51 +0400

    Added from in style page

--- components/Style/From.vue
+++ components/Style/From.vue
@@ -4,7 +4,20 @@
       <Lnk href="https://tailwindcss.com/docs/gradient-color-stops" class="whitespace-pre">from-  via-  to-</Lnk>
     </div>
     <div class="content">
-      text
+      <div class="grid grid-cols-3 gap-2">
+        <div>
+          <div class="text-red-600">{color}</div>
+          <div class="h-14 mt-1 rounded bg-gradient-to-r from-blue-700 to-red-600"></div>
+        </div>
+        <div>
+          <TwClass name="to-transparent" tooltip="--tw-gradient-to: transparent">transparent</TwClass>
+          <div class="h-14 mt-1 rounded bg-gradient-to-r from-blue-700 to-transparent"></div>
+        </div>
+        <div>
+          <TwClass name="to-current" tooltip="--tw-gradient-to: currentColor">current</TwClass>
+          <div class="h-14 mt-1 rounded bg-gradient-to-r from-blue-700 to-current text-red-600"></div>
+        </div>
+      </div>
     </div>
   </Card>
 </template>
