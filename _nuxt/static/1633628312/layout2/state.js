bb2c03ef73af767479de1b866e784d0a8becab28
utopianknight <utopianknight@users.noreply.github.com>
2021-10-08 09:04:10 +0000

    deploy: 32481f7ff191ccb994b9e2b2f7837cee4f9e2b25

--- _nuxt/static/1633628312/layout2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FTailwind-CSS-Cheatsheet\u002F_nuxt\u002Fstatic\u002F1633628312",layout:"default",error:a,serverRendered:true,routePath:"\u002Flayout2",config:{_app:{basePath:"\u002FTailwind-CSS-Cheatsheet\u002F",assetsPath:"\u002FTailwind-CSS-Cheatsheet\u002F_nuxt\u002F",cdnURL:a}}}}(null));
