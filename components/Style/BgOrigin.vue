32481f7ff191ccb994b9e2b2f7837cee4f9e2b25
utopianknight <thesithlord2007+github@gmail.com>
2021-10-08 13:02:32 +0400

    Add background origin to style

--- components/Style/BgOrigin.vue
+++ components/Style/BgOrigin.vue
@@ -1,10 +1,23 @@
 <template>
   <Card>
     <div class="prefix">
-      <Lnk href="https://www.example.com">bg-origin-</Lnk>
+      <Lnk href="https://tailwindcss.com/docs/background-origin">bg-origin-</Lnk>
     </div>
     <div class="content">
-      text
+      <div class="grid grid-cols-3 gap-2">
+        <div>
+          <TwClass name="bg-origin-border" tooltip="background-origin: border-box">border</TwClass>
+          <div class="h-16 mt-1 rounded p-3 bg-no-repeat border-4 border-dashed border-red-500 bg-origin-border" style="background-image: url(placeholder-light-blue.svg)"></div>
+        </div>
+        <div>
+          <TwClass name="bg-origin-padding" tooltip="background-origin: padding-box">padding</TwClass>
+          <div class="h-16 mt-1 rounded p-3 bg-no-repeat border-4 border-dashed border-red-500 bg-origin-padding" style="background-image: url(placeholder-light-blue.svg)"></div>
+        </div>
+        <div>
+          <TwClass name="bg-origin-content" tooltip="background-origin: content-box">content</TwClass>
+          <div class="h-16 mt-1 rounded p-3 bg-no-repeat border-4 border-dashed border-red-500 bg-origin-content" style="background-image: url(placeholder-light-blue.svg)"></div>
+        </div>
+      </div>
     </div>
   </Card>
 </template>
