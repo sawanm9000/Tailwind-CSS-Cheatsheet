f45662fd7f4a2d688124f37f3583a7de8a5f4f81
Sawan M <sawanm9000@gmail.com>
2021-10-17 17:59:26 +0400

    Improved layout of placeholder card in typography

--- components/Typography/Placeholder.vue
+++ components/Typography/Placeholder.vue
@@ -1,13 +1,19 @@
 <template>
   <Card>
     <div class="prefix"><Lnk href="https://tailwindcss.com/docs/placeholder-color">placeholder-</Lnk></div>
-    <div class="content grid grid-cols-2 gap-y-3">
-      <a href="/style" class="text-red-600">{color}</a>
-      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded placeholder-red-600" placeholder="Abcdefgh" />
-      <TwClass class="w-max" name="placeholder-transparent" tooltip="color: transparent">transparent</TwClass>
-      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded placeholder-transparent" placeholder="Abcdefgh" />
-      <TwClass class="w-max" name="placeholder-current" tooltip="color: currentColor">current</TwClass>
-      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded text-emerald-400 placeholder-current" placeholder="Abcdefgh" />
+    <div class="content grid grid-cols-3 gap-2">
+      <div>
+        <a href="/style" class="text-red-600">{color}</a>
+        <input class="font-sans text-sm pl-2 py-0.5 mt-1 bg-coolGray-700 border border-coolGray-400 rounded w-full placeholder-red-600" placeholder="Abcdefgh" />
+      </div>
+      <div>
+        <TwClass class="w-max" name="placeholder-transparent" tooltip="color: transparent">transparent</TwClass>
+        <input class="font-sans text-sm pl-2 py-0.5 mt-1 bg-coolGray-700 border border-coolGray-400 rounded w-full placeholder-transparent" placeholder="Abcdefgh" />
+      </div>
+      <div>
+        <TwClass class="w-max" name="placeholder-current" tooltip="color: currentColor">current</TwClass>
+        <input class="font-sans text-sm pl-2 py-0.5 mt-1 bg-coolGray-700 border border-coolGray-400 rounded w-full text-emerald-400 placeholder-current" placeholder="Abcdefgh" />
+      </div>
     </div>
   </Card>
 </template>
