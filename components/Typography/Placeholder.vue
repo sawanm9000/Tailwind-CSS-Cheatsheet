da7897d162f68ce21ea2ca6f1dad6b8ede6adbad
Sawan M <sawanm9000@gmail.com>
2021-10-17 16:07:04 +0400

    Fixed placeholder color in Typography

--- components/Typography/Placeholder.vue
+++ components/Typography/Placeholder.vue
@@ -2,12 +2,12 @@
   <Card>
     <div class="prefix"><Lnk href="https://tailwindcss.com/docs/placeholder-color">placeholder-</Lnk></div>
     <div class="content grid grid-cols-2 gap-y-3">
+      <a href="/style" class="text-red-600">{color}</a>
+      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded placeholder-red-600" placeholder="Abcdefgh" />
       <TwClass class="w-max" name="placeholder-transparent" tooltip="color: transparent">transparent</TwClass>
       <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded placeholder-transparent" placeholder="Abcdefgh" />
       <TwClass class="w-max" name="placeholder-current" tooltip="color: currentColor">current</TwClass>
-      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded placeholder-current" placeholder="Abcdefgh" />
-      <a href="/style" class="text-red-600">{color}</a>
-      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded placeholder-red-600" placeholder="Abcdefgh" />
+      <input class="font-sans text-sm pl-2 bg-coolGray-700 border rounded text-emerald-400 placeholder-current" placeholder="Abcdefgh" />
     </div>
   </Card>
 </template>
