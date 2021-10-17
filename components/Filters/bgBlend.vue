455a24b36829ad402d6677e811c168993aa3fa80
Sawan M <sawanm9000@gmail.com>
2021-10-17 18:23:38 +0400

    Improved filters page

--- components/Filters/bgBlend.vue
+++ components/Filters/bgBlend.vue
@@ -3,67 +3,67 @@
     <div class="prefix"><Lnk href="https://tailwindcss.com/docs/background-blend-mode">bg-blend-</Lnk></div>
     <div class="content grid grid-cols-4 gap-2">
       <div>
-        <TwClass name="bg-blend-normal" tooltip="background-blend-mode: normal" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">normal</TwClass>
+        <TwClass name="bg-blend-normal" tooltip="background-blend-mode: normal" class="w-max">normal</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-normal" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-multiply" tooltip="background-blend-mode: multiply" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">multiply</TwClass>
+        <TwClass name="bg-blend-multiply" tooltip="background-blend-mode: multiply" class="w-max">multiply</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-multiply" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-screen" tooltip="background-blend-mode: screen" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">screen</TwClass>
+        <TwClass name="bg-blend-screen" tooltip="background-blend-mode: screen" class="w-max">screen</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-screen" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-overlay" tooltip="background-blend-mode: overlay" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">overlay</TwClass>
+        <TwClass name="bg-blend-overlay" tooltip="background-blend-mode: overlay" class="w-max">overlay</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-overlay" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-darken" tooltip="background-blend-mode: darken" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">darken</TwClass>
+        <TwClass name="bg-blend-darken" tooltip="background-blend-mode: darken" class="w-max">darken</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-darken" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-lighten" tooltip="background-blend-mode: lighten" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">lighten</TwClass>
+        <TwClass name="bg-blend-lighten" tooltip="background-blend-mode: lighten" class="w-max">lighten</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-lighten" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-color-dodge" tooltip="background-blend-mode: color-dodge" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">color-dodge</TwClass>
+        <TwClass name="bg-blend-color-dodge" tooltip="background-blend-mode: color-dodge" class="w-max">color-dodge</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-color-dodge" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-color-burn" tooltip="background-blend-mode: color-burn" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">color-burn</TwClass>
+        <TwClass name="bg-blend-color-burn" tooltip="background-blend-mode: color-burn" class="w-max">color-burn</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-color-burn" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-hard-light" tooltip="background-blend-mode: hard-light" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">hard-light</TwClass>
+        <TwClass name="bg-blend-hard-light" tooltip="background-blend-mode: hard-light" class="w-max">hard-light</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-hard-light" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-soft-light" tooltip="background-blend-mode: soft-light" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">soft-light</TwClass>
+        <TwClass name="bg-blend-soft-light" tooltip="background-blend-mode: soft-light" class="w-max">soft-light</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-soft-light" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-difference" tooltip="background-blend-mode: difference" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">difference</TwClass>
+        <TwClass name="bg-blend-difference" tooltip="background-blend-mode: difference" class="w-max">difference</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-difference" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-exclusion" tooltip="background-blend-mode: exclusion" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">exclusion</TwClass>
+        <TwClass name="bg-blend-exclusion" tooltip="background-blend-mode: exclusion" class="w-max">exclusion</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-exclusion" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-hue" tooltip="background-blend-mode: hue" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">hue</TwClass>
+        <TwClass name="bg-blend-hue" tooltip="background-blend-mode: hue" class="w-max">hue</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-hue" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-saturation" tooltip="background-blend-mode: saturation" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">saturation</TwClass>
+        <TwClass name="bg-blend-saturation" tooltip="background-blend-mode: saturation" class="w-max">saturation</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-saturation" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-color" tooltip="background-blend-mode: color" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">color</TwClass>
+        <TwClass name="bg-blend-color" tooltip="background-blend-mode: color" class="w-max">color</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-color" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
       <div>
-        <TwClass name="bg-blend-luminosity" tooltip="background-blend-mode: luminosity" class="w-max backdrop-filter backdrop-blur-sm backdrop-brightness-50 rounded px-1">luminosity</TwClass>
+        <TwClass name="bg-blend-luminosity" tooltip="background-blend-mode: luminosity" class="w-max">luminosity</TwClass>
         <div class="h-14 mt-1 rounded border border-opacity-30 bg-cover bg-red-700 bg-center bg-blend-luminosity" style="background-image: url(placeholder-light-blue.svg)"></div>
       </div>
     </div>
--- components/Filters/dropShadow.vue
+++ components/Filters/dropShadow.vue
@@ -7,31 +7,31 @@
       <div class="flex gap-2">
         <div class="flex-1">
           <TwClass name="drop-shadow-none" tooltip="drop-shadow(0 0 #0000)" class="w-max text-black">-none</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow-none"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow-none"></ImgPng>
         </div>
         <div class="flex-1">
           <TwClass name="drop-shadow-sm" tooltip="--tw-drop-shadow: drop-shadow(0 1px 1px rgba(0,0,0,0.05))" class="w-max text-black">-sm</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow-sm"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow-sm"></ImgPng>
         </div>
         <div class="flex-1">
           <TwClass name="drop-shadow" tooltip="--tw-drop-shadow: drop-shadow(0 1px 2px rgba(0, 0, 0, 0.1)) drop-shadow(0 1px 1px rgba(0, 0, 0, 0.06))" class="opacity-0">.</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow"></ImgPng>
         </div>
         <div class="flex-1">
           <TwClass name="drop-shadow-md" tooltip="drop-shadow(0 4px 3px rgba(0, 0, 0, 0.07)) drop-shadow(0 2px 2px rgba(0, 0, 0, 0.06))" class="w-max text-black">-md</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow-md"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow-md"></ImgPng>
         </div>
         <div class="flex-1">
           <TwClass name="drop-shadow-lg" tooltip="drop-shadow(0 10px 8px rgba(0, 0, 0, 0.04)) drop-shadow(0 4px 3px rgba(0, 0, 0, 0.1))" class="w-max text-black">-lg</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow-lg"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow-lg"></ImgPng>
         </div>
         <div class="flex-1">
           <TwClass name="drop-shadow-xl" tooltip="drop-shadow(0 20px 13px rgba(0, 0, 0, 0.03)) drop-shadow(0 8px 5px rgba(0, 0, 0, 0.08))" class="w-max text-black">-xl</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow-xl"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow-xl"></ImgPng>
         </div>
         <div class="flex-1">
           <TwClass name="drop-shadow-2xl" tooltip="drop-shadow(0 25px 25px rgba(0, 0, 0, 0.15))" class="w-max text-black">-2xl</TwClass>
-          <ImgPng class="h-14 w-full rounded mt-1 filter drop-shadow-2xl"></ImgPng>
+          <ImgPng class="h-14 w-full rounded mt-1 filter brightness-200 saturate-50 drop-shadow-2xl"></ImgPng>
         </div>
       </div>
     </div>
