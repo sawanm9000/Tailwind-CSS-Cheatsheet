aac7979d8de2011e980aa288b02c5c4e327ceab1
Sawan M <sawanm9000@gmail.com>
2021-10-18 21:49:16 +0400

    Improved animate card

--- components/transform/animate.vue
+++ components/transform/animate.vue
@@ -27,13 +27,15 @@
         <div>
           <TwClass name="animate-pulse" tooltip="" class="w-max">pulse</TwClass>
           <div class="h-20 pt-3">
-            <div class="w-14 h-14 mt-1 bg-coolGray-600 rounded animate-pulse"></div>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14 mt-1 animate-pulse text-amber-500" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+            </svg>
           </div>
         </div>
         <div>
           <TwClass name="animate-bounce" tooltip="" class="w-max">bounce</TwClass>
           <div class="h-20 pt-3">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 animate-bounce text-amber-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-14 h-14 animate-bounce text-lightBlue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3" />
             </svg>
           </div>
