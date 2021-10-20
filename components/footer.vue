8123a07508053b1aa0aa38d05e7c3ec751bf08a6
Sawan M <sawanm9000@gmail.com>
2021-10-20 20:55:39 +0400

    Improved look of footer on small displays

--- components/footer.vue
+++ components/footer.vue
@@ -1,12 +1,12 @@
 <template>
   <footer class="text-coolGray-400 text-center text-sm w-full md:flex md:justify-center" :class="inDrawer ? '' : 'p-5 bg-black bg-opacity-50'">
-    <div class="container flex gap-x-3 justify-center md:justify-between" :class="inDrawer ? 'flex-col' : ''">
+    <div class="container flex gap-x-3 justify-center md:justify-between" :class="inDrawer ? 'flex-col' : 'flex-col md:flex-row'">
       <div class="order-2 text-left">
         <div class="inline">Created & maintained by <a href="https://sawanm9000.github.io" class="text-coolGray-100">Sawan M</a> - </div>
         <div id="donate" class="inline"><span class="w-max whitespace-nowrap"><a href="http://paypal.me/sawanm9000" class="text-red-600 font-medium">❤ Donate</a></span></div>
       </div>
       <div class="text-left" :class="inDrawer ? 'order-1' : 'order-3'">Tailwind CSS v2.2</div>
-      <div class="order-4 text-right">Tailwind CSS Cheatsheet is not affiliated with Tailwind Labs</div>
+      <div class="order-4 text-left md:text-right">Tailwind CSS Cheatsheet is not affiliated with Tailwind Labs</div>
     </div>
   </footer>
 </template>
