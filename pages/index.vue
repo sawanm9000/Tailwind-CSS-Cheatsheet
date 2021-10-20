d7f8d74b0246503d70ddb760043bcd9bf0b1dc7d
Sawan M <sawanm9000@gmail.com>
2021-10-20 11:37:57 +0400

    Minor changes to footer

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="min-h-full flex flex-col justify-between items-center">
 
     <header class="w-full flex justify-center">
-      <div class="flex justify-center container max-w-screen-xl sm:justify-between p-4">
+      <div class="flex justify-center container sm:justify-between p-4">
         <div class="flex gap-x-2 items-center">
           <img src="icon.png" alt="Logo" style="height: 32px; width: 32px;">
           <h1 class="text-2xl text-center font-bold">Tailwind CSS Cheatsheet (WIP)</h1>
@@ -19,20 +19,21 @@
     <!-- <input id="search" type="search" name="Search" placeholder="Search (Ctrl+K)" class="w-96 max-w-full px-3 py-2 m-3 bg-coolGray-600"> -->
     <!-- use this: https://github.com/krisk/Fuse -->
 
-    <div class="flex items-center max-w-full px-2 py-24">
+    <div class="flex items-center max-w-full px-2 py-10 3xl:py-24">
       <div class="w-full flex justify-center">
         <Nav button="yes" class="overflow-hidden" />
       </div>
     </div>
 
-    <footer class="p-5 bg-black bg-opacity-50 text-coolGray-400 text-center text-sm w-full md:flex md:justify-between">
-      <div class="flex text-center justify-center">
-        <div>Created & maintained by <a href="https://sawanm9000.github.io" class="text-coolGray-100 pr-2">Sawan M</a>-</div>
-        <div><a href="http://paypal.me/sawanm9000" class="pl-2 text-red-600 font-medium">❤ Donate</a></div>
+    <footer class="p-5 bg-black bg-opacity-50 text-coolGray-400 text-center text-sm w-full md:flex md:justify-center">
+      <div class="container flex gap-x-3 justify-center md:justify-between text-center">
+        <div class="text-left">
+          <div class="inline">Created & maintained by <a href="https://sawanm9000.github.io" class="text-coolGray-100">Sawan M</a> - </div>
+          <div class="inline"><span class="w-max whitespace-nowrap"><a href="http://paypal.me/sawanm9000" class="text-red-600 font-medium">❤ Donate</a></span></div>
+        </div>
+        <div>Tailwind CSS v2.2</div>
+        <div class="text-right">Tailwind CSS Cheatsheet is not affiliated with Tailwind Labs</div>
       </div>
-      <div>Optimized for 1080p displays</div>
-      <div>Tailwind CSS v2.1</div>
-      <div>Tailwind CSS Cheatsheet is not affiliated with Tailwind Labs</div>
     </footer>
 
   </div>
