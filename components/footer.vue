73acffd75f5752b1a3ed135be0da222a44d60c9a
Sawan M <sawanm9000@gmail.com>
2021-10-21 17:12:55 +0400

    Added links to documentation and github in footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,9 @@
 <template>
   <footer class="text-coolGray-400 text-center text-sm w-full md:flex md:justify-center" :class="inDrawer ? '' : 'p-5 bg-black bg-opacity-50'">
+    <div class="mb-4 md:hidden text-coolGray-100">
+      <span class="mr-8"><a href="https://tailwindcss.com/docs">Documentation</a></span>
+      <a href="https://github.com/sawanm9000/Tailwind-CSS-Cheatsheet">GitHub</a>
+    </div>
     <div class="container flex gap-x-3 justify-center md:justify-between" :class="inDrawer ? 'flex-col' : 'flex-col md:flex-row'">
       <div class="order-2 text-left">
         <div class="inline">Created & maintained by <a href="https://sawanm9000.github.io" class="text-coolGray-100">Sawan M</a> - </div>
