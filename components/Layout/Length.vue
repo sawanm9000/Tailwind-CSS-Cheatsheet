5e206b55bccdbb1fc4a9c0efc9c1bccdfb9e70c2
Sawan M <sawanm9000@gmail.com>
2021-11-08 16:46:18 +0400

    Added tooltip to lengths card

--- components/Layout/Length.vue
+++ components/Layout/Length.vue
@@ -4,27 +4,27 @@
 		<div class="content overflow-x-auto relative">
 			<div class="relative h-20 pl-10 pr-6 w-max">
 				<div class="top-sizes">
-					<div class="w-96 h-4 -mt-4">96</div>
-					<div class="w-80 h-4 -mt-4">80</div>
-					<div class="w-72 h-4 -mt-4">72</div>
-					<div class="w-64 h-4 -mt-4">64</div>
-					<div class="w-56 h-4 -mt-4">56</div>
-					<div class="w-48 h-4 -mt-4">48</div>
-					<div class="w-40 h-4 -mt-4">40</div>
-					<div class="w-32 h-4 -mt-4">32</div>
-					<div class="w-24 h-4 -mt-4">24</div>
-					<div class="w-16 h-16 border-r border-coolGray-500 pr-0.5 -mt-16">16</div>
-					<div class="w-14 h-12 border-r border-coolGray-500 pr-0.5 -mt-12">14</div>
-					<div class="w-12 h-8 border-r border-coolGray-500 pr-0.5 -mt-8">12</div>
-					<div class="w-11 h-4 -mt-4">11</div>
-          <div class="w-6 h-12 border-r border-coolGray-500 pr-0.5 -mt-12">6</div>
-					<div class="w-5 h-8 border-r border-coolGray-500 pr-0.5 -mt-8">5</div>
-					<div class="w-4 h-4 -mt-4">4</div>
-          <div class="w-1.5 h-16 border-r border-coolGray-500 pr-0.5 flex content-end justify-end -mt-16">1.5</div>
-					<div class="w-1 h-12 border-r border-coolGray-500 pr-0.5 flex content-end justify-end -mt-12">1</div>
-					<div class="w-0.5 h-8 border-r border-coolGray-500 pr-0.5 flex content-end justify-end -mt-8">0.5</div>
-					<div class="w-px h-4 -mt-4 flex content-end justify-end">px</div>
-					<div class="w-0 h-4 flex content-end justify-end">0</div>
+					<TwClass name="96" tooltip="width: 24rem" class="w-96 h-4 -mt-4">96</TwClass>
+					<TwClass name="80" tooltip="width: 20rem" class="w-80 h-4 -mt-4">80</TwClass>
+					<TwClass name="72" tooltip="width: 18rem" class="w-72 h-4 -mt-4">72</TwClass>
+					<TwClass name="64" tooltip="width: 16rem" class="w-64 h-4 -mt-4">64</TwClass>
+					<TwClass name="56" tooltip="width: 14rem" class="w-56 h-4 -mt-4">56</TwClass>
+					<TwClass name="48" tooltip="width: 12rem" class="w-48 h-4 -mt-4">48</TwClass>
+					<TwClass name="40" tooltip="width: 10rem" class="w-40 h-4 -mt-4">40</TwClass>
+					<TwClass name="32" tooltip="width: 8rem" class="w-32 h-4 -mt-4">32</TwClass>
+					<TwClass name="24" tooltip="width: 6rem" class="w-24 h-4 -mt-4">24</TwClass>
+					<TwClass name="16" tooltip="width: 4rem" class="w-16 h-16 border-r border-coolGray-500 pr-0.5 -mt-16">16</TwClass>
+					<TwClass name="14" tooltip="width: 3.5rem" class="w-14 h-12 border-r border-coolGray-500 pr-0.5 -mt-12">14</TwClass>
+					<TwClass name="12" tooltip="width: 3rem" class="w-12 h-8 border-r border-coolGray-500 pr-0.5 -mt-8">12</TwClass>
+					<TwClass name="11" tooltip="width: 2.75rem" class="w-11 h-4 -mt-4">11</TwClass>
+          <TwClass name="6" tooltip="width: 1.5rem" class="w-6 h-12 border-r border-coolGray-500 pr-0.5 -mt-12">6</TwClass>
+					<TwClass name="5" tooltip="width: 1.25rem" class="w-5 h-8 border-r border-coolGray-500 pr-0.5 -mt-8">5</TwClass>
+					<TwClass name="4" tooltip="width: 1rem" class="w-4 h-4 -mt-4">4</TwClass>
+          <TwClass name="1.5" tooltip="width: 0.375rem" class="w-1.5 h-16 border-r border-coolGray-500 pr-0.5 flex content-end justify-end -mt-16">1.5</TwClass>
+					<TwClass name="1" tooltip="width: 0.25rem" class="w-1 h-12 border-r border-coolGray-500 pr-0.5 flex content-end justify-end -mt-12">1</TwClass>
+					<TwClass name="0.5" tooltip="width: 0.125rem" class="w-0.5 h-8 border-r border-coolGray-500 pr-0.5 flex content-end justify-end -mt-8">0.5</TwClass>
+					<TwClass name="px" tooltip="width: 1px" class="w-px h-4 -mt-4 flex content-end justify-end">px</TwClass>
+					<TwClass name="0" tooltip="width: 0px" class="w-0 h-4 flex content-end justify-end">0</TwClass>
 				</div>
 				<div class="bars w-96">
 					<div class="w-96"></div>
@@ -63,20 +63,20 @@
 					<div class="w-px"></div>
 				</div>
 				<div class="bottom-sizes">
-					<div class="w-60">60</div>
-					<div class="w-52">52</div>
-					<div class="w-44">44</div>
-					<div class="w-36">36</div>
-					<div class="w-28">28</div>
-					<div class="w-20">20</div>
-          <div class="w-10 h-16 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">10</div>
-					<div class="w-9 h-12 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">9</div>
-					<div class="w-8 h-8 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">8</div>
-					<div class="w-7">7</div>
-					<div class="w-3.5 h-16 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">3.5</div>
-					<div class="w-3 h-12 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">3</div>
-					<div class="w-2.5 h-8 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">2.5</div>
-					<div class="w-2">2</div>
+					<TwClass name="60" tooltip="width: 15rem" class="w-60">60</TwClass>
+					<TwClass name="52" tooltip="width: 13rem" class="w-52">52</TwClass>
+					<TwClass name="44" tooltip="width: 11rem" class="w-44">44</TwClass>
+					<TwClass name="36" tooltip="width: 9rem" class="w-36">36</TwClass>
+					<TwClass name="28" tooltip="width: 7rem" class="w-28">28</TwClass>
+					<TwClass name="20" tooltip="width: 5rem" class="w-20">20</TwClass>
+          <TwClass name="10" tooltip="width: 2.5rem" class="w-10 h-16 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">10</TwClass>
+					<TwClass name="9" tooltip="width: 2.25rem" class="w-9 h-12 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">9</TwClass>
+					<TwClass name="8" tooltip="width: 2rem" class="w-8 h-8 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">8</TwClass>
+					<TwClass name="7" tooltip="width: 1.75rem" class="w-7">7</TwClass>
+					<TwClass name="3.5" tooltip="width: 0.875rem" class="w-3.5 h-16 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">3.5</TwClass>
+					<TwClass name="3" tooltip="width: 0.75rem" class="w-3 h-12 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">3</TwClass>
+					<TwClass name="2.5" tooltip="width: 0.625rem" class="w-2.5 h-8 border-r border-coolGray-500 pr-0.5 flex content-end items-end justify-end">2.5</TwClass>
+					<TwClass name="2" tooltip="width: 0.5rem" class="w-2">2</TwClass>
 				</div>
 			</div>
 		</div>
