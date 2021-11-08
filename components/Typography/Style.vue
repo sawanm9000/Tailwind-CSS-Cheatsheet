489012d1b3d479e2521e5ab59f999086df76ff11
Sawan M <sawanm9000@gmail.com>
2021-11-08 18:01:40 +0400

    Updated font style card

--- components/Typography/Style.vue
+++ components/Typography/Style.vue
@@ -1,15 +1,16 @@
 <template>
   <Card>
     <div class="content">
-      <div class="title"><Lnk href="https://tailwindcss.com/docs/font-style">Style</Lnk></div>
+      <div class="title"><Lnk href="https://tailwindcss.com/docs/font-style">Font style</Lnk></div>
       <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="italic" tooltip="font-style: italic" class="flex-1 w-max">italic</TwClass>
         <div class="flex-1 font-sans text-base italic">Abcdefgh</div>
       </div>
-      <div class="flex gap-x-2 items-center mb-5">
+      <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="not-italic" tooltip="font-style: normal" class="flex-1 w-max">not-italic</TwClass>
         <div class="flex-1 font-sans text-base not-italic">Abcdefgh</div>
       </div>
+      <div class="title mt-2"><Lnk href="https://tailwindcss.com/docs/text-decoration">Text decoration</Lnk></div>
       <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="underline" tooltip="text-decoration: underline" class="flex-1 w-max">underline</TwClass>
         <div class="flex-1 font-sans text-base underline">Abcdefgh</div>
@@ -18,10 +19,11 @@
         <TwClass name="no-underline" tooltip="text-decoration: none" class="flex-1 w-max">no-underline</TwClass>
         <div class="flex-1 font-sans text-base no-underline">Abcdefgh</div>
       </div>
-      <div class="flex gap-x-2 items-center mb-5">
+      <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="line-through" tooltip="text-decoration: line-through" class="flex-1 w-max">line-through</TwClass>
         <div class="flex-1 font-sans text-base line-through">Abcdefgh</div>
       </div>
+      <div class="title mt-2"><Lnk href="https://tailwindcss.com/docs/text-transform">Text transform</Lnk></div>
       <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="uppercase" tooltip="text-transform: uppercase" class="flex-1 w-max">uppercase</TwClass>
         <div class="flex-1 font-sans text-base uppercase">abcdefgh</div>
@@ -34,10 +36,11 @@
         <TwClass name="capitalize" tooltip="text-transform: capitalize" class="flex-1 w-max">capitalize</TwClass>
         <div class="flex-1 font-sans text-base capitalize">abcdefgh</div>
       </div>
-      <div class="flex gap-x-2 items-center mb-5">
+      <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="normal-case" tooltip="text-transform: none" class="flex-1 w-max">normal-case</TwClass>
         <div class="flex-1 font-sans text-base normal-case">Abcdefgh</div>
       </div>
+      <div class="title mt-2"><Lnk href="https://tailwindcss.com/docs/text-overflow">Text overflow</Lnk></div>
       <div class="flex justify-between gap-x-2 mb-1">
         <TwClass name="truncate" tooltip="overflow: hidden" class="w-max">truncate</TwClass>
         <div class="font-sans text-base w-12 mr-24 truncate">Abcdefgh</div>
@@ -50,10 +53,11 @@
         <TwClass name="overflow-clip" tooltip="text-overflow: clip" class="w-max">overflow-clip</TwClass>
         <div class="font-sans text-base w-12 mr-24 overflow-hidden overflow-clip">Abcdefgh</div>
       </div>
-      <div class="flex gap-x-2 items-center mb-5">
+      <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="max-w-prose" tooltip="max-width: 65ch" class="flex-1 w-max">max-w-prose</TwClass>
         <div class="flex-1 text-sm max-w-prose text-emerald-500">65ch</div>
       </div>
+      <div class="title mt-2"><Lnk href="https://tailwindcss.com/docs/font-smoothing">Font smoothing</Lnk></div>
       <div class="flex gap-x-2 items-center mb-1">
         <TwClass name="antialiased" tooltip="-webkit-font-smoothing: antialiased" class="flex-1 w-max">antialiased</TwClass>
         <div class="flex-1 font-sans text-base antialiased">Abcdefgh</div>
