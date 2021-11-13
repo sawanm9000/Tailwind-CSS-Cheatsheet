c724eb5863737f8ed6f7ff432069ac805ffd18cc
sawanm9000 <sawanm9000@users.noreply.github.com>
2021-11-13 06:33:04 +0000

    deploy: ead525d9544c2e91589957aabd90fbd761303b7d

--- _nuxt/static/1636380223/transform/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FTailwind-CSS-Cheatsheet\u002F_nuxt\u002Fstatic\u002F1636380223",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftransform",config:{_app:{basePath:"\u002FTailwind-CSS-Cheatsheet\u002F",assetsPath:"\u002FTailwind-CSS-Cheatsheet\u002F_nuxt\u002F",cdnURL:a}}}}(null));
